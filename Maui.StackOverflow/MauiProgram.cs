3abb75e8d3782272e45466139eb2ea1b22b9db8c
Stephen Quan <squan@esri.com>
2024-08-19 10:03:28 +1000

    Add MCT and fix SolidBrandButton issue

--- Maui.StackOverflow/MauiProgram.cs
+++ Maui.StackOverflow/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Maui.StackOverflow;
 
@@ -9,6 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.RegisterSample<RgbColorPage>(nameof(RgbColorPage))
 			.RegisterSample<ThemePage>(nameof(ThemePage))
 			.RegisterSample<UserThemePage>(nameof(UserThemePage))
