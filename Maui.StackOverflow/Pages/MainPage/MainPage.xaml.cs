d94b2de1ae492d2f45ec392e20b33abb34e17bb3
Stephen Quan <squan@esri.com>
2024-11-17 15:29:41 +1100

    Update MainPage

--- Maui.StackOverflow/Pages/MainPage/MainPage.xaml.cs
+++ Maui.StackOverflow/Pages/MainPage/MainPage.xaml.cs
@@ -1,34 +1,25 @@
-﻿namespace Maui.StackOverflow;
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace Maui.StackOverflow;
 
 public partial class MainPage : ContentPage
 {
-    //int count = 0;
-
     public MainPage()
     {
         InitializeComponent();
     }
-
-    //private void OnCounterClicked(object sender, EventArgs e)
-    //{
-    //    count++;
-
-    //    if (count == 1)
-    //        CounterBtn.Text = $"Clicked {count} time";
-    //    else
-    //        CounterBtn.Text = $"Clicked {count} times";
-
-    //    SemanticScreenReader.Announce(CounterBtn.Text);
-    //}
 
 	/// <summary>
 	/// 
 	/// </summary>
 	/// <param name="sender"></param>
 	/// <param name="e"></param>
-    async void OnNavigate(object sender, EventArgs e)
+    public async void OnNavigate(object sender, EventArgs e)
     {
 		Button btn = (Button)sender;
+		btn.IsEnabled = false;
+		await Task.Delay(50);
         await Shell.Current.GoToAsync(btn.CommandParameter.ToString());
+		btn.IsEnabled = true;
     }
 }
