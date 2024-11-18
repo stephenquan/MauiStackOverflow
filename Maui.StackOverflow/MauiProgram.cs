dbd3a5afc069023083dfb3281a16a53296f01a6c
Stephen Quan <squan@esri.com>
2024-11-18 16:47:24 +1100

    ScrollView workaround

--- Maui.StackOverflow/MauiProgram.cs
+++ Maui.StackOverflow/MauiProgram.cs
@@ -23,6 +23,7 @@ public static class MauiProgram
 			.RegisterSample<ExpanderPage>(nameof(ExpanderPage), "Expander page")
 			.RegisterSample<FileSaverPage>(nameof(FileSaverPage), "FileSaver page")
 			.RegisterSample<FlexPage>(nameof(FlexPage), "FlexLayout page")
+			.RegisterSample<ImageClipPage>(nameof(ImageClipPage), "Demonstrates an image aspect clip")
 			.RegisterSample<ImageMapPage>(nameof(ImageMapPage), "Demonstrates an image map")
 			.RegisterSample<LocalizedPage>(nameof(LocalizedPage), "Localized page")
 			.RegisterSample<NumericEntryPage>(nameof(NumericEntryPage), "Demonstrates NumericEntry control")
