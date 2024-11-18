8988ac09dcbe11a7bf11918fcd7117ec866afd41
Stephen Quan <squan@esri.com>
2024-11-18 19:33:15 +1100

    .NET MAUI 9

--- Maui.StackOverflow/Pages/MainPage/MainPage.xaml.cs
+++ Maui.StackOverflow/Pages/MainPage/MainPage.xaml.cs
@@ -1,25 +1,38 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System.Reflection;
 
 namespace Maui.StackOverflow;
 
 public partial class MainPage : ContentPage
 {
-    public MainPage()
-    {
-        InitializeComponent();
-    }
+	public string? MauiVersion { get; set; } = "0.0.0";
+
+	public MainPage()
+	{
+		var assembly = typeof(MainPage).Assembly;
+		foreach (var attribute in assembly.GetCustomAttributes<AssemblyMetadataAttribute>())
+		{
+			switch (attribute.Key)
+			{
+				case "Assembly.Metadata.Attribute.MauiVersion":
+					MauiVersion = attribute.Value;
+					break;
+			}
+		}
+
+		InitializeComponent();
+	}
 
 	/// <summary>
 	/// 
 	/// </summary>
 	/// <param name="sender"></param>
 	/// <param name="e"></param>
-    public async void OnNavigate(object sender, EventArgs e)
-    {
+	public async void OnNavigate(object sender, EventArgs e)
+	{
 		Button btn = (Button)sender;
 		btn.IsEnabled = false;
 		await Task.Delay(50);
-        await Shell.Current.GoToAsync(btn.CommandParameter.ToString());
+		await Shell.Current.GoToAsync(btn.CommandParameter.ToString());
 		btn.IsEnabled = true;
-    }
+	}
 }
